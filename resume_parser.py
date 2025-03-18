c5ccc31a43e2d7779c30559025b5b0ef8a106ede
JMT <tjuanmiguel116@gmail.com>
2025-03-18 16:22:45 +0800

    making the resume filter using fuzzy match and spacy matcher

--- resume_parser.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.append("scripts")
-
-from nlp_processor import DisplaySents, DisplayEnts, DisplayTokenDetails, GetSents, GetEnts, GetTokenDetails
-from file_processor import DOCtoText, PDFtoText, CreateTextFile
--- scripts/file_processor.py
+++ scripts/file_processor.py
@@ -1,30 +1,14 @@
 import pdfplumber
-import asyncio
 from docx import Document
 
-async def DOCtoText(filename):
+def read_docs(filename):
     doc = Document(filename)
     paragraphs = [paragraph.text for paragraph in doc.paragraphs if paragraph.text.strip()]
     text = "\n".join(paragraphs)
-    file = filename[6:(len(filename)-5)]
-    with open(f"texts/{file}_docx.txt", "w", encoding="utf-8") as f:
-        f.write(text)
-    print("Document converted to txt file")
+    return text
 
-async def PDFtoText(filename):
+def read_pdf(filename):
+    """Extracts text from a PDF file."""
     with pdfplumber.open(filename) as pdf:
-        sentences = [page.extract_text() for page in pdf.pages]
-        texts = "\n".join(sentences)
-
-        file = filename[6:(len(filename)-4)]
-        with open(f"texts/{file}_pdf.txt", "w", encoding="utf-8") as f:
-            f.write(texts)
-        print("PDF converted to txt file")
-
-async def CreateTextFile(text, filename):
-    with open(f"texts/{filename}", "w", encoding="utf-8") as f:
-        f.write(text)
-    print("Created txt file")
-
-async def CreateDocument(text, filename):
-    print("something")
+        text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
+    return text
